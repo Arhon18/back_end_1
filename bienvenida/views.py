6600b9c0a6661730f1c94a81e2247e8dd32794bc
Arhon Espinoza <arhonespinozadonoso@gmail.com>
2026-08-25 12:59:01 -0400

    hola

--- bienvenida/views.py
+++ bienvenida/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+from .models import Producto
+
 # Create your views here.
 def inicio(request):
     return HttpResponse("Hola mundo desde Django")
+
+def lista_productos(request):
+    preoductos = Producto.objects.all()
+    return render()
